87ea7d26a44767b64298ff9cdfb329ed04f378e8
NekoYasha <autornekoyasha@gmail.com>
2023-04-17 10:26:09 -0300

    added hibernate dependency

---
+++ src/main/java/br/com/ifba/giovaneneves/RegistrationProjectWithSpring/controller/Facade.java
@@ -0,0 +1,2 @@
+package br.com.ifba.giovaneneves.RegistrationProjectWithSpring.controller;public class Facade {
+}
---
+++ src/main/java/br/com/ifba/giovaneneves/RegistrationProjectWithSpring/dao/GenereicDAO.java
@@ -0,0 +1,2 @@
+package br.com.ifba.giovaneneves.RegistrationProjectWithSpring.dao;public class GenereicDAO {
+}
