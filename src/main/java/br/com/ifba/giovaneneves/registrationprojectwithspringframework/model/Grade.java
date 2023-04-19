da5b604dd517c40007d424293be329a86a9d2a29
NekoYasha <autornekoyasha@gmail.com>
2023-04-19 09:55:38 -0300

    removed unused annotations

--- src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/model/Grade.java
+++ src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/model/Grade.java
@@ -13,12 +13,8 @@ public class Grade {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
 
-    @Column(nullable = true)
     private float grade1;
-
-    @Column(nullable = true)
     private float grade2;
 
-    @Column(nullable = true)
     private float grade3;
 }
