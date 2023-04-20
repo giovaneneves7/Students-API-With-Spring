33741d024f523977903e03c840159cda87722057
NekoYasha <autornekoyasha@gmail.com>
2023-04-20 13:39:11 -0300

    user repository added

--- src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/repositories/StudentRepository.java
+++ src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/repositories/StudentRepository.java
@@ -1,8 +1,11 @@
 package br.com.ifba.giovaneneves.registrationprojectwithspringframework.repositories;
 
 import br.com.ifba.giovaneneves.registrationprojectwithspringframework.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
 }
--- src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/repositories/UserRepository.java
+++ src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/repositories/UserRepository.java
@@ -1,2 +1,13 @@
-package br.com.ifba.giovaneneves.registrationprojectwithspringframework.repositories;public interface UserRepository {
+package br.com.ifba.giovaneneves.registrationprojectwithspringframework.repositories;
+
+import br.com.ifba.giovaneneves.registrationprojectwithspringframework.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findUserByEmailAndPassword(String email, String password);
 }
