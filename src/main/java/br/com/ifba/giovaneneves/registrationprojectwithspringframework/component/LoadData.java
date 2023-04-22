ac8a8782e26e97accc57c44dcbe646103663cba8
NekoYasha <autornekoyasha@gmail.com>
2023-04-22 10:49:15 -0300

    Load component created

--- src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/component/LoadData.java
+++ src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/component/LoadData.java
@@ -1,7 +1,7 @@
-package br.com.ifba.giovaneneves.registrationprojectwithspringframework;
+package br.com.ifba.giovaneneves.registrationprojectwithspringframework.component;
 
 import br.com.ifba.giovaneneves.registrationprojectwithspringframework.model.Role;
-import br.com.ifba.giovaneneves.registrationprojectwithspringframework.repositories.RoleRepository;
+import br.com.ifba.giovaneneves.registrationprojectwithspringframework.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
