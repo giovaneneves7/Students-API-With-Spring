89765e318dfb09fa56b7cf1b9687a900412f16d7
NekoYasha <autornekoyasha@gmail.com>
2023-04-22 10:21:30 -0300

    Role Repository created

--- src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/repositories/RoleRepository.java
+++ src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/repositories/RoleRepository.java
@@ -1,2 +1,8 @@
-package br.com.ifba.giovaneneves.registrationprojectwithspringframework.repositories;public class RoleRepository {
+package br.com.ifba.giovaneneves.registrationprojectwithspringframework.repositories;
+
+import br.com.ifba.giovaneneves.registrationprojectwithspringframework.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
 }
