0deaf770282a2d1d5ce435cb1ff744a42ae5dfa6
NekoYasha <autornekoyasha@gmail.com>
2023-04-22 10:20:55 -0300

    extends to AbstractEntity added

--- src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/model/Grade.java
+++ src/main/java/br/com/ifba/giovaneneves/registrationprojectwithspringframework/model/Grade.java
@@ -7,11 +7,7 @@ import lombok.Data;
 @Entity
 @Table(name="grades")
 @Data
-public class Grade {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int id;
+public class Grade extends AbstractEntity{
 
     private float grade1;
     private float grade2;
