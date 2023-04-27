683525e0d94649f56e59840843bf89bfe0587c41
NekoYasha <autornekoyasha@gmail.com>
2023-04-27 13:28:18 -0300

    user not found exception created

--- src/main/java/br/com/ifba/giovaneneves/sms/infrastructure/exceptions/user/UserNotFoundException.java
+++ src/main/java/br/com/ifba/giovaneneves/sms/infrastructure/exceptions/user/UserNotFoundException.java
@@ -1,2 +1,10 @@
-package br.com.ifba.giovaneneves.sms.infrastructure.exceptions.user;public class UserNotFoundException {
+package br.com.ifba.giovaneneves.sms.infrastructure.exceptions.user;
+
+public class UserNotFoundException extends Exception{
+
+    public UserNotFoundException(String message){
+
+        super(message);
+
+    }
 }
