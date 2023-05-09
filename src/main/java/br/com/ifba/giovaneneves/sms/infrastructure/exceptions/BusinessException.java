88dc1af3e43d8e9c332a65ef90f2019eecb58425
NekoYasha <autornekoyasha@gmail.com>
2023-05-09 11:41:40 -0300

    created universal exception

--- src/main/java/br/com/ifba/giovaneneves/sms/infrastructure/exceptions/BusinessException.java
+++ src/main/java/br/com/ifba/giovaneneves/sms/infrastructure/exceptions/BusinessException.java
@@ -1,2 +1,25 @@
-package br.com.ifba.giovaneneves.sms.infrastructure.exceptions;public class BusinessException {
+package br.com.ifba.giovaneneves.sms.infrastructure.exceptions;
+
+public class BusinessException extends Exception{
+
+    public BusinessException() {
+    }
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BusinessException(Throwable cause) {
+        super(cause);
+    }
+
+    public BusinessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+
 }
