a448137408f27eefb4fb4f5d478e33c5f1b4dbef
NekoYasha <autornekoyasha@gmail.com>
2023-05-09 11:42:19 -0300

    created grade resource

--- src/main/java/br/com/ifba/giovaneneves/sms/api/resource/student/model/GradeResource.java
+++ src/main/java/br/com/ifba/giovaneneves/sms/api/resource/student/model/GradeResource.java
@@ -1,2 +1,23 @@
-package br.com.ifba.giovaneneves.sms.api.resource.student.model;public class GradeResource {
+//======================================{ PACKAGE }======================================//
+package br.com.ifba.giovaneneves.sms.api.resource.student.model;
+//======================================{ PACKAGE }======================================//
+
+//======================================{ IMPORTS }======================================//
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+//======================================{ END IMPORTS }======================================//
+
+@Data
+public class GradeResource {
+
+    //======================================{ ATTRIBUTES }======================================//
+    @JsonProperty("grade_1")
+    private String grade1;
+
+    @JsonProperty("grade_2")
+    private String grade2;
+
+    @JsonProperty("grade_3")
+    private String grade3;
+
 }
