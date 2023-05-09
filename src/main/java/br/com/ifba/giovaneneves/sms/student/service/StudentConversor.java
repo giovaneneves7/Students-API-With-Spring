0d27a16aeb5b1e8f0a93375e460c731cfc1ef983
NekoYasha <autornekoyasha@gmail.com>
2023-05-09 11:40:29 -0300

    created student conversor service

--- src/main/java/br/com/ifba/giovaneneves/sms/student/service/StudentConversor.java
+++ src/main/java/br/com/ifba/giovaneneves/sms/student/service/StudentConversor.java
@@ -1,5 +1,11 @@
 package br.com.ifba.giovaneneves.sms.student.service;
 
+import br.com.ifba.giovaneneves.sms.api.resource.student.model.GradeResource;
+import br.com.ifba.giovaneneves.sms.api.resource.student.model.StudentResource;
+import br.com.ifba.giovaneneves.sms.infrastructure.exceptions.BusinessException;
+import br.com.ifba.giovaneneves.sms.student.grade.model.Grade;
+import br.com.ifba.giovaneneves.sms.student.model.Student;
+
 import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
@@ -7,24 +13,72 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Component
-public class DataConversor {
+public class StudentConversor {
 
-    public static Date convertToDate(String stringToBeConverted){
+    private static final String INVALID_BIRTH_DATE = "The date must be valid";
+    private static final String WRONG_NUMBER_FORMAT = "The data types are not compatible";
 
-        Date convertedDate;
+    public static Student ConvertStudent(StudentResource studentResource) throws BusinessException{
+
+        Student student = new Student();
+
+        student.setName(studentResource.getName());
+        student.setRegistrationNumber(studentResource.getRegistrationNumber());
+
+        student.setBirthDate(checkDate(studentResource.getBirthDate()));
+        student.setAcademicYear(convertToInt(studentResource.getAcademicYear()));
+        student.setGrade(convertToGrade(studentResource.getGrades()));
+
+        student.setAverageGrades();
+
+        return student;
+    }
+
+    private static Date checkDate(String stringToBeConverted) throws BusinessException{
 
         try{
 
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            convertedDate = dateFormat.parse(stringToBeConverted);
+            Date convertedDate = dateFormat.parse(stringToBeConverted);
             return convertedDate;
 
         } catch(ParseException ex){
 
-            ex.printStackTrace();
-            return null;
+            throw new BusinessException(INVALID_BIRTH_DATE);
+
         }
-        
+
+    }
+
+    private static int convertToInt(String stringToBeConverted) throws BusinessException{
+
+        try{
+
+            Integer convertedInt = Integer.parseInt(stringToBeConverted);
+            return convertedInt;
+
+        }catch(NumberFormatException ex){
+
+            throw new BusinessException(WRONG_NUMBER_FORMAT);
+        }
+    }
+
+    private static Grade convertToGrade(GradeResource gradeResource) throws BusinessException{
+
+        try{
+
+            Grade grades = new Grade();
+            grades.setGrade1(Float.parseFloat(gradeResource.getGrade1()));
+            grades.setGrade2(Float.parseFloat(gradeResource.getGrade2()));
+            grades.setGrade3(Float.parseFloat(gradeResource.getGrade3()));
+
+            return grades;
+
+        } catch(Exception ex){
+
+            throw new BusinessException(WRONG_NUMBER_FORMAT);
+
+        }
     }
 }
 
