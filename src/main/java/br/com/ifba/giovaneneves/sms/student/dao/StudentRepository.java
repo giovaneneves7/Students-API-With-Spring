606e53f5d96bd69546d68c17b1edf0266c20b40f
NekoYasha <autornekoyasha@gmail.com>
2023-05-11 11:11:57 -0300

    Added comments

--- src/main/java/br/com/ifba/giovaneneves/sms/student/dao/StudentRepository.java
+++ src/main/java/br/com/ifba/giovaneneves/sms/student/dao/StudentRepository.java
@@ -1,9 +1,13 @@
+//======================================{ PACKAGE }======================================//
 package br.com.ifba.giovaneneves.sms.student.dao;
+//======================================{ END PACKAGE }======================================//
 
+//======================================{ IMPORTS }======================================//
 import br.com.ifba.giovaneneves.sms.student.model.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+//======================================{ END IMPORTS }======================================//
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
